fac43021c2a08731f4d43e6defc28e0389bc925f
Taku Kitamura <tikutaku0128@icloud.com>
2018-07-06 23:20:06 +0900

    fix

--- svm.py
+++ svm.py
@@ -6,11 +6,11 @@ import tensorflow as tf
 from matplotlib import pyplot as plt
 
 
-def return_kernel_estimator(features_number):
+def return_kernel_estimator(data):
     optimizer = tf.train.FtrlOptimizer(learning_rate=1.2)
 
     kernel_mapper = tf.contrib.kernel_methods.RandomFourierFeatureMapper(
-        input_dim=features_number, output_dim=1)
+        input_dim=data[1]-1, output_dim=1)
 
     kernel_mappers = {
         tf.contrib.layers.real_valued_column('feature'): [kernel_mapper]}
@@ -65,8 +65,8 @@ def return_input_fn(train_data, evaluate_data, predict_data):
     #     print(probabilities)
 
 
-def cross_validation_estimate(data, train_steps, evaluate_steps, block_number, features_number):
-    estimator = return_kernel_estimator(features_number)
+def cross_validation_estimate(data, train_steps, evaluate_steps, block_number):
+    estimator = return_kernel_estimator(data)
     split_data = np.array_split(data, block_number)
 
     test_data = split_data[0]
@@ -142,6 +142,89 @@ def cross_validation_estimate(data, train_steps, evaluate_steps, block_number, f
 
     return evaluate_average_loss, evaluate_average_accuracy, test_average_loss, test_average_accuracy
 
+
+def show_learnig_graph(data):
+    target_data_length_x = np.array([])
+    evaluate_average_loss_y = np.array([])
+    test_average_loss_y = np.array([])
+
+
+    print(data)
+    division_number = 10
+    features_number = data.shape[1] - 1
+    for i in range(1, division_number + 1):
+        np.random.shuffle(data)
+        split_data = np.array_split(data, division_number)
+        target_data = np.vstack(split_data[:i])
+        target_data_length = len(target_data)
+        evaluate_average_loss, evaluate_average_accuracy, test_average_loss, test_average_accuracy = cross_validation_estimate(target_data, 2000, 100, 10, features_number)
+        print('Data length is {0}'.format(target_data_length))
+        print()
+        print('Loss evaluate average is {0}'.format(evaluate_average_loss))
+        print('Accuracy evaluate average is {0}'.format(evaluate_average_accuracy))
+        print()
+        print('Loss test average is {0}'.format(test_average_loss))
+        print('Accuracy test average is {0}'.format(test_average_accuracy))
+
+        print()
+        target_data_length_x = np.append(target_data_length_x, target_data_length)
+        evaluate_average_loss_y = np.append(evaluate_average_loss_y, evaluate_average_loss)
+        test_average_loss_y = np.append(test_average_loss_y, test_average_loss)
+
+    x = target_data_length_x
+    y1 = evaluate_average_loss_y
+    y2 = test_average_loss_y
+
+    print(x)
+    print(y1)
+    print(y2)
+
+    plt.scatter(x , y1)
+    plt.scatter(x , y2)
+    plt.plot(x, np.poly1d(np.polyfit(x, y1, 3))(x), label='d=3')
+    plt.plot(x, np.poly1d(np.polyfit(x, y2, 3))(x), label='d=3')
+
+    plt.show()
+
+def predict(analysis_data, predict_data, train_steps, evaluate_steps):
+    np.random.shuffle(analysis_data)
+    data_length = len(analysis_data)
+    train_data_rate = 0.7
+
+    if train_data_rate > 0 and train_data_rate < 1:
+        split_index = math.floor(data_length * train_data_rate)
+        train_data = analysis_data[:split_index]
+        evaluate_data = analysis_data[split_index:]
+
+        if len(train_data) == 0 or len(evaluate_data) == 0:
+            raise ValueError("トレイニング用のデータ割合が不正です。")
+    else:
+        raise ValueError("トレイニング用のデータ割合が不正です。")
+
+    input_fn_train, input_fn_evaluate, input_fn_predict = return_input_fn(
+        train_data, evaluate_data, predict_data)
+
+    estimator = return_kernel_estimator(analysis_data)
+    estimator.fit(input_fn=input_fn_train, steps=train_steps)
+
+    test_results = estimator.evaluate(
+        input_fn=input_fn_evaluate, steps=evaluate_steps)
+
+    test_accuracy = test_results['accuracy']
+    test_loss = test_results['loss']
+
+    print('Accuracy is {0}'.format(test_accuracy))
+    print('Loss is {0}'.format(test_loss))
+
+    predict_results = estimator.predict(input_fn=input_fn_predict)
+    logits = predict_results["logits"]
+
+    print('Logits is {0}'.format(logits))
+
+    with tf.Session():
+        probabilities = 1 / (1 + np.exp(-logits))
+        print('Probabilities is {0}'.format(probabilities))
+
 # Warning非表示
 # 参考: https://qiita.com/KEINOS/items/4c66eeda4347f8c13abb
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
@@ -153,75 +236,8 @@ tf.logging.set_verbosity(tf.logging.ERROR)
 
 # np.set_printoptions(threshold=np.inf)
 
-# sess = tf.Session()
-#
 data = np.array(
     np.loadtxt("/Users/kitamurataku/work/SVM/data.csv", delimiter=","), "float64")
 
-# data = data[:, [0, 1, 2, 4, 5, 3]]
-# print(data)
-
-target_data_length_x = np.array([])
-evaluate_average_loss_y = np.array([])
-test_average_loss_y = np.array([])
-
-
-print(data)
-division_number = 10
-features_number = data.shape[1] - 1
-for i in range(1, division_number + 1):
-    np.random.shuffle(data)
-    split_data = np.array_split(data, division_number)
-    target_data = np.vstack(split_data[:i])
-    target_data_length = len(target_data)
-    evaluate_average_loss, evaluate_average_accuracy, test_average_loss, test_average_accuracy = cross_validation_estimate(target_data, 2000, 100, 10, features_number)
-    print('Data length is {0}'.format(target_data_length))
-    print()
-    print('Loss evaluate average is {0}'.format(evaluate_average_loss))
-    print('Accuracy evaluate average is {0}'.format(evaluate_average_accuracy))
-    print()
-    print('Loss test average is {0}'.format(test_average_loss))
-    print('Accuracy test average is {0}'.format(test_average_accuracy))
-
-    print()
-    target_data_length_x = np.append(target_data_length_x, target_data_length)
-    evaluate_average_loss_y = np.append(evaluate_average_loss_y, evaluate_average_loss)
-    test_average_loss_y = np.append(test_average_loss_y, test_average_loss)
-
-x = target_data_length_x
-y1 = evaluate_average_loss_y
-y2 = test_average_loss_y
-
-print(x)
-print(y1)
-print(y2)
-
-plt.scatter(x , y1)
-plt.scatter(x , y2)
-plt.plot(x, np.poly1d(np.polyfit(x, y1, 3))(x), label='d=3')
-plt.plot(x, np.poly1d(np.polyfit(x, y2, 3))(x), label='d=3')
-
-plt.show()
-
-# analysis_data = data
-#
-# np.random.shuffle(analysis_data)
-#
-# data_length = len(analysis_data)
-#
-# train_data_rate = 0.7
-#
-# if train_data_rate > 0 and train_data_rate < 1:
-#     split_index = math.floor(data_length * train_data_rate)
-#     train_data = analysis_data[:split_index]
-#     evaluate_data = analysis_data[split_index:]
-#
-#     if len(train_data) == 0 or len(evaluate_data) == 0:
-#         raise ValueError("トレイニング用のデータ割合が不正です。")
-# else:
-#     raise ValueError("トレイニング用のデータ割合が不正です。")
-
-
-# predict_data = np.array([[2016, 1, 12, 1, 6800, 6864, 6755, 6755, 12126300]])
-#
-# return_input_fn(train_data, evaluate_data, predict_data, 1)
+show_learnig_graph(data)
+predict(data, [[2016,2,9,5.7,8.7,2.5,2,4.2,4.3,9.3,242,15.1,242,2]], 2000, 100)
